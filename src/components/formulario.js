9cc3695c62a15b97f1a04df9176746cd4534f32e
davidmenlop <davidmenlop@gmail.com>
2021-09-20 12:34:30 -0500

    ok

---
+++ src/components/formulario.js
@@ -0,0 +1,123 @@
+import React, { Fragment, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+export default function Formulario({crearCita}) {
+  //Crear State de Citas
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  const [error, actualizarError] = useState(false);
+
+  //Funcion que se ejecuta en cada input
+  const handleChange = (e) => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //Extraer los valores
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  //Cuando el usuario agregue la cita.
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Validar
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      actualizarError(true);
+      return;
+    }
+
+    //Eliminar mensaje previo
+    actualizarError(false);
+
+    //Asignar un ID
+    cita.id = uuidv4();
+
+    //Crear la cita
+    crearCita(cita)
+
+    //Reiniciar el form
+    actualizarCita({
+        mascota: "",
+        propietario: "",
+        fecha: "",
+        hora: "",
+        sintomas: "",
+      });
+  };
+
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+
+      {error ? (
+        <p className="alerta-error">Todos los campos son obligatorios</p>
+      ) : null}
+
+      <form onSubmit={handleSubmit}>
+        <label>Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+          onChange={handleChange}
+          value={mascota}
+        />
+
+        <label>Nombre propietario</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Propietario"
+          onChange={handleChange}
+          value={propietario}
+        />
+
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={handleChange}
+          value={fecha}
+        />
+
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={handleChange}
+          value={hora}
+        />
+
+        <label>Sintomas</label>
+        <textarea
+          className="u-full-width"
+          name="sintomas"
+          onChange={handleChange}
+          value={sintomas}
+        ></textarea>
+
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
+      </form>
+    </Fragment>
+  );
+}
